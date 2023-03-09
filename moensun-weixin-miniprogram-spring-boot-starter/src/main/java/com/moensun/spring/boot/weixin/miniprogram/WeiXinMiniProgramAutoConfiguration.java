b3dd0710503b12621a634b39cab409607d9a2fb4
Bane.Shi <fengxiaotx@163.com>
2023-03-09 16:13:43 +0800

    [dev] wx cache

--- moensun-weixin-miniprogram-spring-boot-starter/src/main/java/com/moensun/spring/boot/weixin/miniprogram/WeiXinMiniProgramAutoConfiguration.java
+++ moensun-weixin-miniprogram-spring-boot-starter/src/main/java/com/moensun/spring/boot/weixin/miniprogram/WeiXinMiniProgramAutoConfiguration.java
@@ -3,9 +3,11 @@ package com.moensun.spring.boot.weixin.miniprogram;
 import com.moensun.weixin.commons.WeiXinCache;
 import com.moensun.weixin.commons.WeiXinConfig;
 import com.moensun.weixin.miniprogram.MiniProgram;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.ConstructorArgumentValues;
@@ -16,54 +18,77 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 import java.util.Map;
+
+@RequiredArgsConstructor
 @Configuration(value = "com.moensun.spring.boot.weixin.miniprogram.WeiXinMiniProgramAutoConfiguration")
 @ConditionalOnProperty(prefix = "weixin.miniprogram", value = "enabled", havingValue = "true")
 @EnableConfigurationProperties(value = {WeiXinMiniProgramConfProperties.class})
-public class WeiXinMiniProgramAutoConfiguration implements EnvironmentAware, BeanFactoryPostProcessor {
+public class WeiXinMiniProgramAutoConfiguration {
 
-    private WeiXinMiniProgramConfProperties confProperties;
+    private final WeiXinMiniProgramConfProperties confProperties;
 
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
-        WeiXinCache weiXinCache = beanFactory.getBean(WeiXinCache.class);
-        Map<String, WeiXinMiniProgramInstanceProperties> instancePropertiesMap = confProperties.getInstance();
-        if (MapUtils.isEmpty(instancePropertiesMap)) {
-            WeiXinConfig weiXinConfig = new WeiXinConfig();
-            weiXinConfig.setAppId(confProperties.getAppId());
-            weiXinConfig.setSecret(confProperties.getSecret());
-            weiXinConfig.setWeiXinCache(weiXinCache);
-            ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
-            argumentValues.addIndexedArgumentValue(0, weiXinConfig);
-            registerBean(beanFactory, argumentValues, MiniProgram.class.getName());
-        } else {
-            instancePropertiesMap.forEach((k, v) -> {
-                WeiXinConfig weiXinConfig = new WeiXinConfig();
-                weiXinConfig.setAppId(v.getAppId());
-                weiXinConfig.setSecret(v.getSecret());
-                weiXinConfig.setWeiXinCache(weiXinCache);
-                String baneName = StringUtils.isBlank(v.getName()) ? k : v.getName();
-                ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
-                argumentValues.addIndexedArgumentValue(0, weiXinConfig);
-                registerBean(beanFactory, argumentValues, baneName);
-            });
+
+    @Configuration
+    @Import(value = {AutoConfiguredRegistrar.class})
+    public static class AutoConfiguredRegistrarConfiguration implements InitializingBean {
+        @Override
+        public void afterPropertiesSet() throws Exception {
+
         }
     }
 
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.confProperties = Binder.get(environment).bind("weixin.miniprogram", WeiXinMiniProgramConfProperties.class).get();
-    }
+    public static class AutoConfiguredRegistrar implements EnvironmentAware, BeanFactoryPostProcessor {
 
-    private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
-        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
-        beanDefinition.setBeanClass(MiniProgram.class);
-        beanDefinition.setBeanClassName(MiniProgram.class.getName());
-        beanDefinition.setConstructorArgumentValues(argumentValues);
-        beanFactory.registerBeanDefinition(beanName, beanDefinition);
+        private WeiXinMiniProgramConfProperties confProperties;
+
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+            DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
+            WeiXinCache weiXinCache;
+            if (beanFactory.getBeanNamesForType(WeiXinCache.class).length != 0) {
+                weiXinCache = beanFactory.getBean(WeiXinCache.class);
+            } else {
+                weiXinCache = null;
+            }
+            Map<String, WeiXinMiniProgramInstanceProperties> instancePropertiesMap = confProperties.getInstance();
+            if (MapUtils.isEmpty(instancePropertiesMap)) {
+                WeiXinConfig weiXinConfig = new WeiXinConfig();
+                weiXinConfig.setAppId(confProperties.getAppId());
+                weiXinConfig.setSecret(confProperties.getSecret());
+                weiXinConfig.setWeiXinCache(weiXinCache);
+                ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
+                argumentValues.addIndexedArgumentValue(0, weiXinConfig);
+                registerBean(beanFactory, argumentValues, MiniProgram.class.getName());
+            } else {
+                instancePropertiesMap.forEach((k, v) -> {
+                    WeiXinConfig weiXinConfig = new WeiXinConfig();
+                    weiXinConfig.setAppId(v.getAppId());
+                    weiXinConfig.setSecret(v.getSecret());
+                    weiXinConfig.setWeiXinCache(weiXinCache);
+                    String baneName = StringUtils.isBlank(v.getName()) ? k : v.getName();
+                    ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
+                    argumentValues.addIndexedArgumentValue(0, weiXinConfig);
+                    registerBean(beanFactory, argumentValues, baneName);
+                });
+            }
+        }
+
+        @Override
+        public void setEnvironment(Environment environment) {
+            this.confProperties = Binder.get(environment).bind("weixin.miniprogram", WeiXinMiniProgramConfProperties.class).get();
+        }
+
+        private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
+            GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+            beanDefinition.setBeanClass(MiniProgram.class);
+            beanDefinition.setBeanClassName(MiniProgram.class.getName());
+            beanDefinition.setConstructorArgumentValues(argumentValues);
+            beanFactory.registerBeanDefinition(beanName, beanDefinition);
+        }
     }
 
 }
--- moensun-weixin-offiaccount-spring-boot-starter/src/main/java/com/moensun/spring/boot/weixin/offiaccount/WeiXinOfficeAccountAutoConfiguration.java
+++ moensun-weixin-offiaccount-spring-boot-starter/src/main/java/com/moensun/spring/boot/weixin/offiaccount/WeiXinOfficeAccountAutoConfiguration.java
@@ -6,6 +6,7 @@ import com.moensun.weixin.offiaccount.OfficeAccount;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.ConstructorArgumentValues;
@@ -16,6 +17,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 import java.util.Map;
@@ -23,47 +25,65 @@ import java.util.Map;
 @Configuration(value = "com.moensun.spring.boot.weixin.offiaccount.WeiXinOfficeAccountAutoConfiguration")
 @ConditionalOnProperty(prefix = "weixin.offiaccount", value = "enabled", havingValue = "true")
 @EnableConfigurationProperties(value = {WeiXinOfficeAccountConfProperties.class})
-public class WeiXinOfficeAccountAutoConfiguration implements EnvironmentAware, BeanFactoryPostProcessor {
-    private WeiXinOfficeAccountConfProperties confProperties;
+public class WeiXinOfficeAccountAutoConfiguration {
 
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
-        WeiXinCache weiXinCache = beanFactory.getBean(WeiXinCache.class);
-        Map<String, WeiXinOfficeAccountInstanceProperties> instancePropertiesMap = confProperties.getInstance();
-        if (MapUtils.isEmpty(instancePropertiesMap)) {
-            WeiXinConfig weiXinConfig = new WeiXinConfig();
-            weiXinConfig.setAppId(confProperties.getAppId());
-            weiXinConfig.setSecret(confProperties.getSecret());
-            weiXinConfig.setWeiXinCache(weiXinCache);
-            ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
-            argumentValues.addIndexedArgumentValue(0, weiXinConfig);
-            registerBean(beanFactory, argumentValues, OfficeAccount.class.getName());
-        } else {
-            instancePropertiesMap.forEach((k, v) -> {
-                WeiXinConfig weiXinConfig = new WeiXinConfig();
-                weiXinConfig.setAppId(v.getAppId());
-                weiXinConfig.setSecret(v.getSecret());
-                weiXinConfig.setWeiXinCache(weiXinCache);
-                String baneName = StringUtils.isBlank(v.getName()) ? k : v.getName();
-                ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
-                argumentValues.addIndexedArgumentValue(0, weiXinConfig);
-                registerBean(beanFactory, argumentValues, baneName);
-            });
+
+    @Configuration
+    @Import(value = {AutoConfiguredRegistrar.class})
+    public static class AutoConfiguredRegistrarConfiguration implements InitializingBean {
+        @Override
+        public void afterPropertiesSet() throws Exception {
+
         }
     }
 
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.confProperties = Binder.get(environment).bind("weixin.offiaccount", WeiXinOfficeAccountConfProperties.class).get();
-    }
+    public static class AutoConfiguredRegistrar implements EnvironmentAware, BeanFactoryPostProcessor {
+        private WeiXinOfficeAccountConfProperties confProperties;
 
-    private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
-        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
-        beanDefinition.setBeanClass(OfficeAccount.class);
-        beanDefinition.setBeanClassName(OfficeAccount.class.getName());
-        beanDefinition.setConstructorArgumentValues(argumentValues);
-        beanFactory.registerBeanDefinition(beanName, beanDefinition);
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+            DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
+            WeiXinCache weiXinCache;
+            if (beanFactory.getBeanNamesForType(WeiXinCache.class).length != 0) {
+                weiXinCache = beanFactory.getBean(WeiXinCache.class);
+            } else {
+                weiXinCache = null;
+            }
+            Map<String, WeiXinOfficeAccountInstanceProperties> instancePropertiesMap = confProperties.getInstance();
+            if (MapUtils.isEmpty(instancePropertiesMap)) {
+                WeiXinConfig weiXinConfig = new WeiXinConfig();
+                weiXinConfig.setAppId(confProperties.getAppId());
+                weiXinConfig.setSecret(confProperties.getSecret());
+                weiXinConfig.setWeiXinCache(weiXinCache);
+                ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
+                argumentValues.addIndexedArgumentValue(0, weiXinConfig);
+                registerBean(beanFactory, argumentValues, OfficeAccount.class.getName());
+            } else {
+                instancePropertiesMap.forEach((k, v) -> {
+                    WeiXinConfig weiXinConfig = new WeiXinConfig();
+                    weiXinConfig.setAppId(v.getAppId());
+                    weiXinConfig.setSecret(v.getSecret());
+                    weiXinConfig.setWeiXinCache(weiXinCache);
+                    String baneName = StringUtils.isBlank(v.getName()) ? k : v.getName();
+                    ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
+                    argumentValues.addIndexedArgumentValue(0, weiXinConfig);
+                    registerBean(beanFactory, argumentValues, baneName);
+                });
+            }
+        }
+
+        @Override
+        public void setEnvironment(Environment environment) {
+            this.confProperties = Binder.get(environment).bind("weixin.offiaccount", WeiXinOfficeAccountConfProperties.class).get();
+        }
+
+        private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
+            GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+            beanDefinition.setBeanClass(OfficeAccount.class);
+            beanDefinition.setBeanClassName(OfficeAccount.class.getName());
+            beanDefinition.setConstructorArgumentValues(argumentValues);
+            beanFactory.registerBeanDefinition(beanName, beanDefinition);
+        }
     }
 
 }
