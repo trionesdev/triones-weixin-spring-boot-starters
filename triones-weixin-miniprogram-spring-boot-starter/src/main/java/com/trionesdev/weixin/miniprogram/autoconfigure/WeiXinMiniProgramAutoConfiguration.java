86b9ddb818ebdaa248a4660f4e0ec2b14f70f4bb
bane.shi <fengxiaotx@163.com>
2024-08-09 00:44:16 +0800

    feat:init

--- triones-weixin-miniprogram-spring-boot-starter/src/main/java/com/trionesdev/weixin/miniprogram/autoconfigure/WeiXinMiniProgramAutoConfiguration.java
+++ triones-weixin-miniprogram-spring-boot-starter/src/main/java/com/trionesdev/weixin/miniprogram/autoconfigure/WeiXinMiniProgramAutoConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
@@ -31,60 +32,69 @@ public class WeiXinMiniProgramAutoConfiguration {
 
     private final WeiXinMiniProgramProperties confProperties;
 
-
-    @Configuration
-    @Import(value = {AutoConfiguredRegistrar.class})
-    public static class AutoConfiguredRegistrarConfiguration implements InitializingBean {
-        @Override
-        public void afterPropertiesSet() throws Exception {
-
-        }
+    @Bean
+    public WeiXinMiniProgram weiXinMiniProgram(WeiXinCache weiXinCache) {
+        WeiXinConfig weiXinConfig = new WeiXinConfig();
+        weiXinConfig.setAppId(confProperties.getAppId());
+        weiXinConfig.setSecret(confProperties.getSecret());
+        weiXinConfig.setWeiXinCache(weiXinCache);
+        return new WeiXinMiniProgram(weiXinConfig);
     }
 
-    public static class AutoConfiguredRegistrar implements EnvironmentAware, BeanFactoryPostProcessor {
 
-        private WeiXinMiniProgramProperties confProperties;
-
-        @Override
-        public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-            DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
-            WeiXinCache weiXinCache = null;
-            Class<?> cache = confProperties.getCache();
-            if (Objects.nonNull(cache)) {
-                if (WeiXinCache.class.isAssignableFrom(cache)) {
-                    if (beanFactory.getBeanNamesForType(cache).length > 0) {
-                        weiXinCache = (WeiXinCache) beanFactory.getBean(cache);
-                    }
-                } else {
-                    throw new WeiXinException("cache class is not implements from  `com.moensun.weixin.commons.class`");
-                }
-            } else {
-                if (beanFactory.getBeanNamesForType(WeiXinCache.class).length != 0) {
-                    weiXinCache = beanFactory.getBean(WeiXinCache.class);
-                }
-            }
-
-            WeiXinConfig weiXinConfig = new WeiXinConfig();
-            weiXinConfig.setAppId(confProperties.getAppId());
-            weiXinConfig.setSecret(confProperties.getSecret());
-            weiXinConfig.setWeiXinCache(weiXinCache);
-            ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
-            argumentValues.addIndexedArgumentValue(0, weiXinConfig);
-            registerBean(beanFactory, argumentValues, WeiXinMiniProgram.class.getName());
-        }
-
-        @Override
-        public void setEnvironment(Environment environment) {
-            this.confProperties = Binder.get(environment).bind("triones.weixin.miniprogram", WeiXinMiniProgramProperties.class).get();
-        }
-
-        private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
-            GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
-            beanDefinition.setBeanClass(WeiXinMiniProgram.class);
-            beanDefinition.setBeanClassName(WeiXinMiniProgram.class.getName());
-            beanDefinition.setConstructorArgumentValues(argumentValues);
-            beanFactory.registerBeanDefinition(beanName, beanDefinition);
-        }
-    }
+//    @Configuration
+//    @Import(value = {AutoConfiguredRegistrar.class})
+//    public static class AutoConfiguredRegistrarConfiguration implements InitializingBean {
+//        @Override
+//        public void afterPropertiesSet() throws Exception {
+//
+//        }
+//    }
+//
+//    public static class AutoConfiguredRegistrar implements EnvironmentAware, BeanFactoryPostProcessor {
+//
+//        private WeiXinMiniProgramProperties confProperties;
+//
+//        @Override
+//        public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+//            DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
+//            WeiXinCache weiXinCache = null;
+//            Class<?> cache = confProperties.getCache();
+//            if (Objects.nonNull(cache)) {
+//                if (WeiXinCache.class.isAssignableFrom(cache)) {
+//                    if (beanFactory.getBeanNamesForType(cache).length > 0) {
+//                        weiXinCache = (WeiXinCache) beanFactory.getBean(cache);
+//                    }
+//                } else {
+//                    throw new WeiXinException("cache class is not implements from  `com.moensun.weixin.commons.class`");
+//                }
+//            } else {
+//                if (beanFactory.getBeanNamesForType(WeiXinCache.class).length != 0) {
+//                    weiXinCache = beanFactory.getBean(WeiXinCache.class);
+//                }
+//            }
+//
+//            WeiXinConfig weiXinConfig = new WeiXinConfig();
+//            weiXinConfig.setAppId(confProperties.getAppId());
+//            weiXinConfig.setSecret(confProperties.getSecret());
+//            weiXinConfig.setWeiXinCache(weiXinCache);
+//            ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
+//            argumentValues.addIndexedArgumentValue(0, weiXinConfig);
+//            registerBean(beanFactory, argumentValues, WeiXinMiniProgram.class.getName());
+//        }
+//
+//        @Override
+//        public void setEnvironment(Environment environment) {
+//            this.confProperties = Binder.get(environment).bind("triones.weixin.miniprogram", WeiXinMiniProgramProperties.class).get();
+//        }
+//
+//        private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
+//            GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+//            beanDefinition.setBeanClass(WeiXinMiniProgram.class);
+//            beanDefinition.setBeanClassName(WeiXinMiniProgram.class.getName());
+//            beanDefinition.setConstructorArgumentValues(argumentValues);
+//            beanFactory.registerBeanDefinition(beanName, beanDefinition);
+//        }
+//    }
 
 }
